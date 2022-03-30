7868cf88746f6d88f1453924763beb5b79e7b71d
Rohit <rohitkushwaha630@gmail.com>
2022-03-30 10:55:11 +0530

    Add project files.

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+import random 
+
+logging.basicConfig(filename='D:\python_ codes_ and_projects\logfile.log',format="{asctime} {levelname:<8} {message}", style="{", encoding='UTF-8',level=logging.DEBUG)
+
+
+a=random.randint(0,10) 
+b=random.randint(10,15)
+
+def add_num(p,q):
+    s=p+q
+    return s
+
+test= add_num(a,b)
+logging.debug('add: {} + {} = {}'.format(a, b, test))
+logging.info('code exceuted successfully!')
