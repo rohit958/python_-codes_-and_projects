dd960cb3edac58bcffb9c716a538f9b4f6a35a6c
Rohit <rohitkushwaha630@gmail.com>
2022-04-05 16:03:44 +0530

    ignored log ifle

--- test/hello_world.py
+++ test/hello_world.py
@@ -1,8 +1,9 @@
+# testing log files and adding random numbers 
 import logging
 import sys
 import random 
 
-logging.basicConfig(filename='D:\python_ codes_ and_projects\logfile.log',format="{asctime} {levelname:<8} {message}", style="{", encoding='UTF-8',level=logging.DEBUG)
+logging.basicConfig(filename=r"D:\python_ codes_ and_projects\test\logfile.log",format="{asctime} {levelname:<8} {message}", style="{", encoding='UTF-8',level=logging.DEBUG)
 
 
 a=random.randint(0,10) 
