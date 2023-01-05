a1a9901e6cb5dd6ff6b8878ed455aae1887a8a17
rohit958 <78528404+rohit958@users.noreply.github.com>
2023-01-05 13:15:49 +0530

    Create basics.py

---
+++ test/Python_revise/basics.py
@@ -0,0 +1,13 @@
+a,b=2,3
+
+def swap(a,b):
+    temp=0
+    a=temp
+    a=b
+    b=temp
+
+if __name__=='__main__':
+    print("before swapping:",a,b)
+    swap(a,b)
+    print("after swapping:",a,b)
+
