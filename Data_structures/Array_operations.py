617e1223a6d6eeda71ab860bdd4e70bf45ed400d
rohit958 <78528404+rohit958@users.noreply.github.com>
2023-01-05 22:27:35 +0530

    Update Array_operations.py

--- Data_structures/Array_operations.py
+++ Data_structures/Array_operations.py
@@ -1,1 +1,36 @@
+'''
+Array  operations
+1-transersal
+2-insertion
+3-deletion
+4-reverse
+5-search
+6-sort
 
+'''
+
+array=[33,1,5,66,7,9,21,76,88,43]
+size=len(array)
+
+def transversal(arr):
+    print("transersal")
+    for x in range(size):
+        print(array[x],end=' ')
+
+
+def insertion(element,index):
+    
+     
+
+    
+
+def deletion():
+    pass
+
+def reverse():
+    pass
+
+
+
+if __name__=='__main__':
+    transversal(array)
