413d978dfe135ce7cec8fb19bc6379ebad5db2dd
rohit958 <78528404+rohit958@users.noreply.github.com>
2023-01-05 13:15:44 +0530

    Update Bubble_sort.py

--- Algorithms/Bubble_sort.py
+++ Algorithms/Bubble_sort.py
@@ -1,4 +1,33 @@
 '''
+Bubble sort 
+
+time complexity-
+space complexity-
 
 
 '''
+
+import numpy as np
+
+
+numbers=np.array([44,2,23,5,66,97,6,2,1])
+
+def BubbleSort(ar):
+    temp=0
+    for i in range(len(ar)-1):
+        for j in range(len(ar)-1):
+            if ar[j]>ar[j+1]:
+                temp=ar[j]
+                ar[j]=ar[j+1]
+                ar[j+1]=temp
+                
+
+
+def PrintArray(arrray):
+    print(arrray)
+
+
+if __name__=='__main__':
+    PrintArray(numbers)
+    SortedArray=arr.array('i',[BubbleSort(numbers)])
+    PrintArray()
