2d1eabf34b3d101b11e1d05edd92a1cbb5592aab
rohit958 <rohitkushwaha630@gmail.com>
2023-01-09 13:18:36 +0530

    MergeSort final

--- Algorithms/MergeSort.py
+++ Algorithms/MergeSort.py
@@ -9,15 +9,47 @@ Worst TimeComplexity= O(log(n^2))
 numbers = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 
 
-def PrintArray(text, arrray):
+def printArray(text, arrray):
     print(text, arrray)
 
 
-def MergeSort():
-    pass
+def mergeSort(ar):
+    if len(ar) <= 1:
+        return ar
+    mid = int(len(ar) / 2)
+    left = ar[:mid]
+    right = ar[mid:]
+    mergeSort(left)
+    mergeSort(right)
+    mergeSortedArrays(left, right, ar)
+
+
+def mergeSortedArrays(a, b, arr):
+    len_a = len(a)
+    len_b = len(b)
+    i = j = k = 0
+    while i < len_a and j < len_b:
+        if a[i] < + b[j]:
+            arr[k] = a[i]
+            i += 1
+
+        else:
+            arr[k] =b[j]
+            j += 1
+        k += 1
+    while i < len_a:
+        arr[k] = a[i]
+        i += 1
+        k += 1
+
+
+    while j < len_b:
+        arr[k] = b[j]
+        j += 1
+        k += 1
 
 
 if __name__ == '__main__':
-    ('before sorting: ', numbers)
-    MergeSort(numbers)
-    PrintArray("after sorting: ", numbers)
+    printArray('before sorting: ', numbers)
+    mergeSort(numbers)
+    printArray("after Merge sorting: ", numbers)
