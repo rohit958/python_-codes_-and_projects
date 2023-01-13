7b6e120aff7b765893d995ef8d4196f2ebf73736
rohit958 <rohitkushwaha630@gmail.com>
2023-01-13 12:12:15 +0530

    i
    f

---
+++ Data_structures/Queue.py
@@ -0,0 +1,41 @@
+'''
+Queue  operations {FIFO}
+1-Enqueue
+2-Dequeue
+3-isEmpty
+4-isFull
+
+ways to implement using -using libraries, using linked list,using list
+'''
+
+
+'''
+#using list
+Q = []
+
+Q.insert(0, 'Raju')
+Q.insert(1, 'Shayam')
+Q.insert(2, 'Babu Rao')
+Q.insert(3, 'Nanji Bhai')
+Q.insert(4, 'Tiwari Seth')
+
+print(Q)
+print(Q.pop(), '. After Dequeue:', Q)
+print(Q.pop(), '. After Dequeue:', Q)
+print(Q.pop(), '. After Dequeue:', Q)
+'''
+# implementing using collections library
+
+from collections import deque
+
+qe = deque()
+
+qe.appendleft( 'Jetha lal')
+qe.appendleft('Roshan Singh Sodhi')
+qe.appendleft( 'Iyer Idli')
+qe.appendleft( 'Popat lal')
+
+print(qe)
+print(qe.pop())
+print(qe.pop())
+print(qe.pop())
--- Data_structures/Stack.py
+++ Data_structures/Stack.py
@@ -7,8 +7,8 @@ Stack  operations
 '''
 # implement using list
 # other ways to implement-using collections and using queue.queueLIFO libraries
-
-
+'''
+from logging import exception
 stack = []
 
 if __name__ == '__main__':
@@ -22,8 +22,29 @@ if __name__ == '__main__':
     stack.pop()
     stack.pop()
     print(stack)
-    def isEmpty(l):
-        if(l.pop) is None:
-            raise exception("stack is empty")
-    isEmpty(stack)
 
+
+    def isEmpty(l):
+        if len(l) == 0:
+            raise exception("stack is empty")
+
+
+    isEmpty(stack)
+'''
+
+# using collections
+from collections import deque
+
+st = deque()
+
+st.append('Jetha lal')
+st.append('Hans raj hathi')
+st.append('Tarak Mehta')
+st.append('Matka King Mohan lal')
+
+print(st)
+
+print('Popping:',st.pop())
+print('Popping:',st.pop())
+print('Popping:',st.pop())
+print('Popping:',st.pop())
