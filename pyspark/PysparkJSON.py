62c4ecd5936fe474e099a78919bdecc120df771d
rohit958 <rohitkushwaha630@gmail.com>
2023-04-10 15:28:31 +0530

    json

--- pyspark/PysparkJSON.py
+++ pyspark/PysparkJSON.py
@@ -13,15 +13,15 @@ findspark.init()
 # import json
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
-spark = SparkSession.builder.getOrCreate()
 
+spark = SparkSession.builder.getOrCreate()
 
 df = spark.read.option("multiline", "true").json(r"D:\git\python_-codes_-and_projects\sample_JSON\sample2.json")
 df.printSchema()
 df.show()
-df.select('age')
+df.select('age').show()
 
 print("after exploding")
-df2 = df.select(explode(df.address),df.age,df.firstName,df.gender,df.lastName,explode(df.phoneNumbers))
+df2 = df.select(explode(df.address))
 df2.printSchema()
 df2.show()
