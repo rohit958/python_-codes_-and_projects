a82b1be8302ce2a49ab9458b6c47188b54893a16
rohit958 <rohitkushwaha630@gmail.com>
2023-06-26 12:18:23 +0530

    JSON parsing

--- pyspark/PysparkJSON.py
+++ pyspark/PysparkJSON.py
@@ -20,10 +20,13 @@ df = spark.read.option("multiline", "true").json(
     r"D:\git\python_-codes_-and_projects\sample_JSON\Govement_data\City_bus.JSON")
 df1 = df.select(["fields", "data"])
 df1.show(n=5)
-# nested JSOn file parsing and wrting to table in MYSQL
+# nested JSOn file parsing and writing to table in MYSQL
 
 print("after exploding")
 df2 = df1.withColumn('fields', explode(col("fields")))
 df3 = df2.withColumn('data', explode(col("data")))
 df3.printSchema()
-df3.select("*", "fields.label").show()
+# separating columns from data column
+df4 = df3.select(df3.data[0].alias("ID"), df3.data[1].alias("Label"), df3.data[2].alias("Type"))
+
+df4.distinct().orderBy("ID").show()
--- test/c.py
+++ test/c.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
 
-with open(r"D:\git\python_-codes_-and_projects\sample_JSON\Govement_data\City_bus.JSON") as file:
-    data = json.load(file)
-    print(data['fields'])
+with open(r"D:\git\python_-codes_-and_projects\sample_JSON\Govement_data\Monthly_petroleum_consumption.JSON") as file:
+    for line in file:
+        data = json.loads(line)
