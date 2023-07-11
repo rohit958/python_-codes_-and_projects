5afaf2555f54af7e1e76884bc7bdc2f6f457b45c
rohit958 <rohitkushwaha630@gmail.com>
2023-07-11 22:31:55 +0530

    XML

--- pyspark/testpyspark.py
+++ pyspark/testpyspark.py
@@ -1,4 +1,13 @@
-import pandas as pd
+import findspark
 
-df = pd.read_xml("D:\Souce_files\sample_XML_files\datafile.xml")
-print("sample: ", df.head(5))
+findspark.init()
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.read.format("com.databricks.spark.xml").option("rowTag", "DATASET").load(
+    "D:\Souce_files\sample_XML_files\datafile.xml")
+
+df.show(n=5)
