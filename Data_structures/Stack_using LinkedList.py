8651d89f8b0851059d32c1cdcad360e218f8921b
rohit958 <rohitkushwaha630@gmail.com>
2023-09-12 09:58:10 +0530

    leetcode

--- Data_structures/Stack_using LinkedList.py
+++ Data_structures/Stack_using LinkedList.py
@@ -17,6 +17,27 @@ class Stack:
             print(temp.value)
             temp = temp.next
 
+    def push(self,value):
+        NewNode=Node(value)
+
+        if self.height==0:
+            self.top=NewNode
+
+        else:
+            NewNode.next=self.top
+            self.top=NewNode
+        self.height+=1
+
+    def Pop(self):
+        if self.height==0:
+            return None
+        else:
+            temp=self.top
+            self.top=self.top.next
+            temp.next=None
+            self.height-=1
+        return (print("removed",temp.value))
+
 
 Stack_ex = Stack(6)
 Stack_ex.printStack()
