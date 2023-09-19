7e9e780f015fe4cae7039d9ec1c3d351f22c2f49
rohit958 <rohitkushwaha630@gmail.com>
2023-09-19 13:04:12 +0530

    leetcode

--- Algorithms/Insertion_sort.py
+++ Algorithms/Insertion_sort.py
@@ -4,6 +4,7 @@ Insertion Sort Algorithm
 Space Complexity-
 Time Complexity-O(NLog(n))
 Worst TimeComplexity= O(log(n^2))
+cons-unstable
 """
 
 ar = [54, 26, 93, 17, 77, 31, 44, 55, 20]
@@ -15,9 +16,15 @@ def PrintArray(text , arrray):
 
 def InsertionSort( ar):
     for i in range(len(ar)-1):
-        min_inx = i
-        for j in range(i+1,len(ar)-1):
-
+        min_indx = i
+        for j in range(i+1,len(ar)):
+            if ar[j]<ar[min_indx]:
+                min_indx=j
+        if min_indx!=i:
+            temp=ar[i]
+            ar[i]=ar[min_indx]
+            ar[min_indx]=temp
+    return ar
 
 
 if __name__ == '__main__':
