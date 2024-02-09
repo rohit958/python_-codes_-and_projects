f856e0b1bac08ac81144e749f78a83dcdf3b9482
rohit958 <rohitkushwaha630@gmail.com>
2024-02-09 22:19:41 +0530

    leetcode

---
+++ Leetcode_python/ticketLine.py
@@ -0,0 +1,9 @@
+from collections import deque
+def timeRequiredToBuy(tickets, k) -> int:
+    line=deque(tickets)
+
+
+tickets = [2,3,2]
+k = 2
+
+print(tickets[-1])
--- pyspark/pysaprkavro.py
+++ pyspark/pysaprkavro.py
@@ -1,8 +1,1 @@
-import findspark
 
-findspark.init()
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate()
-
-dfav=spark.read.
-#dfav.show()
