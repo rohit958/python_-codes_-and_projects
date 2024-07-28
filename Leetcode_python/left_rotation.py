171135abe0a57378600e6ba56e96b0295f22e436
rohit958 <rohitkushwaha630@gmail.com>
2024-07-28 14:01:02 +0530

    practice

---
+++ Leetcode_python/left_rotation.py
@@ -0,0 +1,10 @@
+#left rotation
+
+list=[1,2,3,4,5]
+
+print(list)
+
+# left rotation for n using list comprehension
+n=3
+rotate=list[n:]+list[:n]
+print("after rotation for {0} times:".format(n),rotate)
