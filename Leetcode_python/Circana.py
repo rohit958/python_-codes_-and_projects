654c781a09acc505475ee455f29a48708000e7d9
rohit958 <rohitkushwaha630@gmail.com>
2024-08-28 18:02:24 +0530

    practice

---
+++ Leetcode_python/Circana.py
@@ -0,0 +1,30 @@
+test_string = "Hello!, World! 123 2024 Welcome' to 2024 Hello!"
+#remove every ! except last one
+#remove ever number except last 2024
+
+
+res=test_string.replace("!","",(test_string.count("!")-1))
+res=res.replace("2024","",(test_string.count("2024")-1))
+res=res.replace("'","")
+
+result=""
+res=res.split()
+
+for word in res:
+    if word.isdigit() and word!='2024':
+        continue
+    result+=" "+word
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
