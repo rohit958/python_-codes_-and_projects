ba6260a9fcb6784936fca625c04496c0de46662f
rohit958 <rohitkushwaha630@gmail.com>
2024-08-30 15:20:01 +0530

    practice

--- Leetcode_python/CodingProblem.py
+++ Leetcode_python/CodingProblem.py
@@ -1,11 +1,16 @@
 test_string = "Hello!, World! 123 2024 Welcome' to 2024 Hello!"
+
+
 #remove every ! except last one
 #remove every number except last 2024
+
 
 
 res=test_string.replace("!","",(test_string.count("!")-1))
 res=res.replace("2024","",(test_string.count("2024")-1))
 res=res.replace("'","")
+
+
 
 result=""
 res=res.split()
